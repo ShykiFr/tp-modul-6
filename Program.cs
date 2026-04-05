8814c9e72ecf51847a837d019a1a85b0898c5ae2
ShykiFr <moh.dzaki.ayatillah@gmail.com>
2026-04-05 15:21:33 +0700

    Menambahkan precondition untuk title di constructor dan count di function IncreasePlayCount

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@
 
     public SayaMusicTrack(string title)
     {
+        // Design Contract untuk title
+        if (title == "")
+        {
+            throw new Exception("Title tidak boleh kosong.");
+        }
+
+        if (title.Length > 100)
+        {
+            throw new Exception("Title tidak boleh lebih dari 100 karakter.");
+        }
+
+        // Isi Contructor
         string placeholder = id.ToString();
         for (int i = 0; i < 5; i++)
         {
@@ -14,14 +26,24 @@
         }
         id = int.Parse(placeholder);
         this.playCount = "0";
+
         this.title = title;
     }
 
     public void IncreasePlayCount(int count)
     {
-        int currentPlayCount = int.Parse(this.playCount);
-        int addedPlayCount = currentPlayCount + count;
-        this.playCount = addedPlayCount.ToString();
+        // Design Contract untuk count
+        if (count > 10000000)
+        {
+            throw new Exception("Jumlah count tidak boleh lebih dari 10.000.000");
+        }
+
+        checked
+        {
+            int currentPlayCount = int.Parse(this.playCount);
+            int addedPlayCount = currentPlayCount + count;
+            this.playCount = addedPlayCount.ToString();
+        }
     }
 
     public void PrintTrackDetails()
@@ -44,13 +66,13 @@ public class Program
         Console.WriteLine();
 
         SayaMusicTrack track2 = new SayaMusicTrack("The weight of the world");
-        track2.IncreasePlayCount(12022004);
+        track2.IncreasePlayCount(1202);
         track2.PrintTrackDetails();
 
         Console.WriteLine();
 
         SayaMusicTrack track3 = new SayaMusicTrack("breathe");
-        track3.IncreasePlayCount(3594234);
+        track3.IncreasePlayCount(3594);
         track3.PrintTrackDetails();
     }
 }
