24940e17f9bd7820c7856df13a2f286cd82cbab7
ShykiFr <moh.dzaki.ayatillah@gmail.com>
2026-04-05 14:13:52 +0700

    membuat kelas SayaMusicTrack dan memaggilnya di kelas Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class SayaMusicTrack
+{
+    Random random = new Random();
+    private int id;
+    private string playCount;
+    private string title;
+
+    public SayaMusicTrack(string title)
+    {
+        string placeholder = id.ToString();
+        for (int i = 0; i < 5; i++)
+        {
+            placeholder += random.Next(10);
+        }
+        id = int.Parse(placeholder);
+        this.playCount = "0";
+        this.title = title;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        int currentPlayCount = int.Parse(this.playCount);
+        int addedPlayCount = currentPlayCount + count;
+        this.playCount = addedPlayCount.ToString();
+    }
+
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine("---TRACK DETAILS---");
+        Console.WriteLine("ID: " + this.id);
+        Console.WriteLine("Play Count: " + this.playCount);
+        Console.WriteLine("Title: " + this.title);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaMusicTrack track1 = new SayaMusicTrack("Less Than Zero");
+        track1.IncreasePlayCount(400000000);
+        track1.PrintTrackDetails();
+
+        Console.WriteLine();
+
+        SayaMusicTrack track2 = new SayaMusicTrack("The weight of the world");
+        track2.IncreasePlayCount(12022004);
+        track2.PrintTrackDetails();
+
+        Console.WriteLine();
+
+        SayaMusicTrack track3 = new SayaMusicTrack("breathe");
+        track3.IncreasePlayCount(3594234);
+        track3.PrintTrackDetails();
+    }
+}
--- obj/Debug/net10.0/TP_MODUL6_103022400090.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_MODUL6_103022400090.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_103022400090")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+662f1124f2cfb5633e4242047381b8fc137637c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_103022400090")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_103022400090")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
